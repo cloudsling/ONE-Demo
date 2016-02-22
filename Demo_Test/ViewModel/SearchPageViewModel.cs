f561407ea0c6100607c0f890343279e4807cf9e9
Hao Ling <lingmengc@outlook.com>
2016-02-22 20:14:10 +0800

    update 34

--- Demo_Test/ViewModel/SearchPageViewModel.cs
+++ Demo_Test/ViewModel/SearchPageViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using Demo;
 using Demo.Common;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -16,32 +15,24 @@ namespace Demo.ViewModel
         public SearchPageViewModel()
         {
             DayObject = new ObservableCollection<DayObject>();
-            for (int i = 0; i < 5; i++)
-            {
-                DayObject.Add(new DayObject
-                {
-                    ByWho = "adafbafdbsfd 作品",
-                    MainString = "bbabababbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
-                    DayImagePath = "ms-appx:///Assets/SunOrNightMode/night1.png"
-                });
-            }
 #if DEBUG
-            //if (DesignMode.DesignModeEnabled)
-            //{
-            //    for (int i = 0; i < 5; i++)
-            //    {
-            //        DayObject.Add(new DayObject
-            //        {
-            //            ByWho = "adafbafdbsfd 作品",
-            //            MainString = "bbabababbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
-            //            DayImagePath = "Assets/SunOrNightMode/night1.png"
-            //        });
-            //    }
-            //}
+            if (DesignMode.DesignModeEnabled)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    DayObject.Add(new DayObject
+                    {
+                        ByWho = "adafbafdbsfd 作品",
+                        MainString = "bbabababbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+                        DayImagePath = "Assets/SunOrNightMode/night1.png"
+                    });
+                }
+            }
 #endif
         }
 
         public ObservableCollection<DayObject> DayObject { get; set; }
 
+        public CommandBase MyProperty { get; set; }
     }
 }
